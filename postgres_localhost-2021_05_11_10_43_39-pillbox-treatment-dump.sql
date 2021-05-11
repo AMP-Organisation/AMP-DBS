07b5d8551379735c5f47a72db8aeb21c1019fcfa
Unknow46 <nathan.sens@hotmail.fr>
2021-05-11 10:47:48 +0200

    :ambulance: modification of the table pillbox and treatment

--- postgres_localhost-2021_05_11_10_43_39-pillbox-treatment-dump.sql
+++ postgres_localhost-2021_05_11_10_43_39-pillbox-treatment-dump.sql
@@ -89,9 +89,10 @@ CREATE TABLE public.treatment (
     id integer NOT NULL,
     name character varying(50) NOT NULL,
     description text,
-    medicine_id integer NOT NULL,
     beginning_date timestamp without time zone DEFAULT (now())::timestamp without time zone NOT NULL,
-    ending_date timestamp without time zone
+    ending_date timestamp without time zone,
+    medicine_id integer[],
+    user_id integer NOT NULL
 );
 
 
@@ -120,28 +121,6 @@ ALTER SEQUENCE public.treatment_id_seq OWNED BY public.treatment.id;
 
 
 --
--- Name: treatment_medicine_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.treatment_medicine_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.treatment_medicine_id_seq OWNER TO postgres;
-
---
--- Name: treatment_medicine_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.treatment_medicine_id_seq OWNED BY public.treatment.medicine_id;
-
-
---
 -- Name: pillbox id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -163,33 +142,27 @@ ALTER TABLE ONLY public.treatment ALTER COLUMN id SET DEFAULT nextval('public.tr
 
 
 --
--- Name: treatment medicine_id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.treatment ALTER COLUMN medicine_id SET DEFAULT nextval('public.treatment_medicine_id_seq'::regclass);
-
-
---
 -- Data for Name: pillbox; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.pillbox (id, name, description, beginning_date, ending_date, list_treatment, user_id) VALUES (1, 'Mon quotidien', 'toujours mes migraines..', '2021-04-07 13:43:51.178', NULL, '{1,2}', 1);
-INSERT INTO public.pillbox (id, name, description, beginning_date, ending_date, list_treatment, user_id) VALUES (2, 'Boost travail', 'ça me motive', '2021-04-07 13:46:08.468', NULL, '{1}', 15);
+INSERT INTO public.pillbox (id, name, description, beginning_date, ending_date, list_treatment, user_id) VALUES (2, 'Boost travail', 'ça me motive', '2021-04-07 13:46:08.468', NULL, '{3}', 15);
+INSERT INTO public.pillbox (id, name, description, beginning_date, ending_date, list_treatment, user_id) VALUES (1, 'Mon quotidien', 'toujours mes migraines..', '2021-04-07 13:43:51.178', NULL, '{3,4,5}', 1);
 
 
 --
 -- Data for Name: treatment; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.treatment (id, name, description, medicine_id, beginning_date, ending_date) VALUES (4, 'Maux de ventre', 'quand je fais chinois la veille', 2, '2021-04-07 13:40:54.698', NULL);
-INSERT INTO public.treatment (id, name, description, medicine_id, beginning_date, ending_date) VALUES (3, 'Maux de tête', 'quand je réfléchis trop (jamais)', 1, '2021-04-07 13:40:54.698', NULL);
+INSERT INTO public.treatment (id, name, description, beginning_date, ending_date, medicine_id, user_id) VALUES (4, 'Maux de ventre', 'quand je fais chinois la veille', '2021-04-07 13:40:54.698', NULL, '{2}', 15);
+INSERT INTO public.treatment (id, name, description, beginning_date, ending_date, medicine_id, user_id) VALUES (5, 'Mirgaine horrible', 'Maux de tête intense', '2021-05-03 09:41:20', NULL, '{10}', 1);
+INSERT INTO public.treatment (id, name, description, beginning_date, ending_date, medicine_id, user_id) VALUES (3, 'Maux de tête', 'quand je réfléchis trop (jamais)', '2021-04-07 13:40:54.698', NULL, '{1}', 1);
 
 
 --
 -- Name: pillbox_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.pillbox_id_seq', 2, true);
+SELECT pg_catalog.setval('public.pillbox_id_seq', 3, true);
 
 
 --
@@ -203,14 +176,7 @@ SELECT pg_catalog.setval('public.pillbox_user_id_seq', 1, false);
 -- Name: treatment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.treatment_id_seq', 4, true);
-
-
---
--- Name: treatment_medicine_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.treatment_medicine_id_seq', 1, false);
+SELECT pg_catalog.setval('public.treatment_id_seq', 5, true);
 
 
 --
@@ -252,11 +218,11 @@ ALTER TABLE ONLY public.pillbox
 
 
 --
--- Name: treatment treatment_medicine__fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: treatment treatment_user__fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.treatment
-    ADD CONSTRAINT treatment_medicine__fk FOREIGN KEY (medicine_id) REFERENCES public.medicine(id);
+    ADD CONSTRAINT treatment_user__fk FOREIGN KEY (user_id) REFERENCES public.users(id);
 
 
 --
